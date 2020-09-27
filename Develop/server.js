2abbb68e156d582abb1aac0cf58d4577fa56b710
Nina Koller <nckoller@gmail.com>
2020-09-26 19:29:36 -0500

    added app.post function

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 // Dependencies
-var express = require("express");
-var path = require("path");
-
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
 //Sets up the Express App
 var app = express();
 var PORT = 3000;
@@ -20,25 +21,47 @@ app.get("/notes", function (req, res) {
 });
 
 // Return the index.html
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 // API Routes
-
-// load Data
-// var savedNotes = require("../db/db");
-
 // GET /api/notes returns db.json
-// app.get("/api/notes", function (req, res) {
-// res.json(savedNotes);
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// POST /api/notes receive a new note to save in the request body
+app.post("/api/notes", function (req, res) {
+  // assigns UUID to the note
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  // Reads the JSON file
+  let notes = fs.readFileSync("./db/db.json", "utf8");
+  // Parses the data to get an array
+  notes = JSON.parse(notes);
+  // Pushes the new note into the array
+  notes.push(newNote);
+  // Writes the new note to the JSON file
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  // Returns the file with the new note added
+  res.json(notes);
+});
+
+// DELETE /api/notes/ by id
+// app.delete("/api/notes", function (req, res) {
+// read all notes from db.json
+// let notes = fs.readFileSync("./db/db.json", "utf8");
+// Parses the data to get an array
+// notes = JSON.parse(notes);
 // });
 
-// POST /api/notes receive a new note to save on the request body
-// add to the db.json file
-// return the new note to the client
-
-// DELETE /api/notes/:id receive a query parameter containing id of note to delete
-// read all notes from db.json
 // remove note with given id
 // rewrite note to db.json
 
