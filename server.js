0f091447d8eaccc921738fe461cdfd67d401d2f7
Nina Koller <nckoller@gmail.com>
2020-09-26 23:08:09 -0500

    fixed package.json, made PORT dynamic in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const uuid = require("uuid");
 //Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //Sets up the Express app to handle data parsing
 app.use(express.static("public"));
